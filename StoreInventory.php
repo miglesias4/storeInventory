be30a8e0fe420c143ec85a07dd53bb9825d4628f
miglesias4 <miglesias4@miners.utep.edu>
2019-09-20 19:44:40 -0600

    Setup UI for adding item in StoreInventory, HTML

--- StoreInventory.php
+++ StoreInventory.php
@@ -1,1 +1,22 @@
-<?php
+<html>
+<h1> Store Inventory</h1>
+
+<?php echo date ("I F D, Y"); ?>
+
+<h3> Enter Product Details</h3>
+
+<fieldset>
+    <legend>Add Item</legend>
+    <label>Name:</label>
+    <input type="text" name="name"
+           required="required"/></label><br />
+    <label>PLU:</label>
+    <input type="text" name="plu"
+           required="required"/></label><br />
+    <label>Image:</label>
+    <input type="file" name="productImage" /><br />
+</fieldset>
+
+<input type="submit" value="Add">
+
+</html>
---
+++ products.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title> Product Inventory </title>
+</head>
+<body>
+    <table border="2" bgcolor="#efefef">
+        <thread>
+            <tr>
+                <th></th>
+                <th scope ="col"> Product Name:</th>
+                <th scope ="col"> PLU:</th>
+            </tr>
+        </thread>
+        <tbody>
+        <!-- PHP code here -->
+        <?php
+
+        echo "php here";
+        ?>
+
+        </tbody>
+    </table>
+
+</body>
+</html>
