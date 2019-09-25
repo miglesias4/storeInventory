2067f5f66fec5f91b666c2b3382d6563bc879244
miglesias4 <miglesias4@miners.utep.edu>
2019-09-25 04:40:34 -0600

    products.php is now set as password prompt

--- products.php
+++ products.php
@@ -1,44 +1,10 @@
 <html>
-<head>
-    <title> Product Inventory </title>
-</head>
-<body>
-    <table border="2" bgcolor="#efefef">
-        <thread>
-            <tr>
-                <th></th>
-                <th scope ="col"> Product Name:</th>
-                <th scope ="col"> PLU:</th>
-            </tr>
-        </thread>
-        <tbody>
-        <?php
-        $file = "products/inventory.txt";
-
-        function add_item($productDesc){
-            global $file;
-            if(!empty($productDesc)){
-                $fp = fopen($file,"a"); //Appends
-                fputs($fp, nl2br($productDesc). "<br/>\n");
-                fclose($fp);
-            }
-        }
-
-        function show_items(){
-            global $file;
-            if(file_exists($file)){
-                $fp = fopen($file, "r"); //Reads
-                while(!feof($fp)){
-                    echo fgets($fp); //Reads line
-                }
-                fclose($fp);
-            }
-        }
-
-        ?>
-
-        </tbody>
-    </table>
-
+<h1> Welcome to Super Market</h1>
+<h3> Sign In:</h3>
+<form action="StoreInventory.php" method=post</form>
+    <input type="password" name="pwd">
+    <p/>
+    <input type="submit" value="Login">
+</form>
 </body>
 </html>
